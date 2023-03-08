e0d7b962c4ed7565f64cae9972281a9104ec196a
Rohan Godha <rgodha24@brophybroncos.org>
2023-03-08 10:08:36 -0700

    init unocss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import '@unocss/reset/tailwind.css';
+</script>
+
+<h1 class="text-2xl ">Welcome to SvelteKit</h1>
+<p class="text-red-600">
+	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+<style uno:preflights uno:safelist global></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { presetWind } from "unocss"
+import unocss from "unocss/vite";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [unocss({
+		presets: [presetWind()],
+		mode: "svelte-scoped",
+	}), sveltekit(),]
 });
