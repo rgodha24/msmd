87ab7f398128711235cc6fec71e9a873e0255792
Rohan Godha <rgodha24@brophybroncos.org>
2023-03-13 22:21:43 -0700

    build error

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,9 +2,7 @@ import { prisma } from '$lib/prisma';
 import type { PageServerLoad } from './$types';
 
 export const load = (async () => {
-	const todos = prisma.todo.findMany();
 	return {
-		todos,
 		session: null
 	};
 }) satisfies PageServerLoad;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,6 @@
 	import type { PageData } from './$types';
 	import Todo from '$lib/Todo.svelte';
 
-	export let data: PageData;
 </script>
 
 <h1 class="text-2xl text-white">Monkey See Monkey Do</h1>
@@ -16,10 +15,5 @@
 <p class="text-white">
 	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
-
-{#each data.todos as todo}
-	<Todo {todo} />
-{/each}
-``
 
 <style uno:preflights uno:safelist global></style>
